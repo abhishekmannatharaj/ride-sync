081bf03037c9d7d0ffa53174f4961c1f8aba8831
Abhishek Mannatharaj <abhishek.mannatharaj@gmail.com>
2026-03-14 22:37:00 +0530

    added push to talk 2.0

--- server.js
+++ server.js
@@ -159,5 +159,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Ride app running on http://localhost:${PORT}`));
 
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Ride app running on http://localhost:${PORT}`));
